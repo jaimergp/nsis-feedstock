4b6b455e8f917f215f0fb4edfc38793736e6dd58
John Kirkham <kirkhamj@janelia.hhmi.org>
2017-11-02 02:29:51 -0400

    Get and install NSIS unicode plugins
    Uses the included Python script to download NSIS unicode plugins. Then
    installs relevant components in the `%PREFIX%` for inclusion in the
    package. These seem to the be the same bits that Anaconda includes.
    Unfortunately `elevate.exe` has not been found yet, so is not being
    included. Everything else is though.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,4 +1,20 @@
 robocopy . "%PREFIX%" /S /XF bld.bat
 if errorlevel NEQ 1 exit 1
 
+cd ..
+if errorlevel 1 exit 1
+
+%PYTHON% "%RECIPE_DIR%\get_plugins.py"
+if errorlevel 1 exit 1
+
+copy "UAC\Plugins\x86-unicode\UAC.dll " "%PREFIX%\Plugins\x86-unicode\"
+if errorlevel 1 exit 1
+copy "Untgz\untgz\unicode\untgz.dll" "%PREFIX%\Plugins\x86-unicode\"
+if errorlevel 1 exit 1
+copy "UnicodePathTest_1.0\Plugin\UnicodePathTest.dll" "%PREFIX%\Plugins\x86-unicode\"
+if errorlevel 1 exit 1
+
+cd "%SRC_DIR%"
+if errorlevel 1 exit 1
+
 exit 0
