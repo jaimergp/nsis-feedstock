d513c8581cfaa4b98709d5148c0147aad11d4abe
Jaime Rodríguez-Guerra <jaimergp@users.noreply.github.com>
2022-08-16 12:14:47 +0200

    rework build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,9 +1,9 @@
-export PREFIX_NSIS=$PREFIX/NSIS
+export PREFIX_NSIS="$PREFIX/NSIS"
 
 pushd binary
 cp -r . "$PREFIX_NSIS"
-rm -rf $PREFIX_NSIS/Docs
-rm -rf $PREFIX_NSIS/Examples
+rm -rf "$PREFIX_NSIS/Docs"
+rm -rf "$PREFIX_NSIS/Examples"
 popd
 
 pushd plugins
@@ -12,25 +12,27 @@ cp "untgz/Plugins/x86-unicode/untgz.dll" "$PREFIX_NSIS/Plugins/x86-unicode/"
 cp "UnicodePathTest/Plugin/UnicodePathTest.dll" "$PREFIX_NSIS/Plugins/x86-unicode/"
 popd
 
-scons_options="CC=\"${CC}\" CXX=\"${CXX}\" APPEND_CCFLAGS=\"${CXXFLAGS}\" APPEND_LINKFLAGS=\"${LDFLAGS}\""
-scons_options+=" SKIPSTUBS=all SKIPPLUGINS=all SKIPUTILS=all SKIPMISC=all"
-scons_options+=" NSIS_CONFIG_CONST_DATA_PATH=yes PREFIX=\"$PREFIX_NSIS\""
-scons_options+=" -Q PATH=\"$PATH\""
-scons_targets="install-compiler"
+extra_scons_options=""
+extra_scons_targets=""
 if [[ $nsis_variant == "log_enabled" ]]; then
-  scons_options+=" NSIS_CONFIG_LOG=yes"
-  scons_targets+=" install-stubs"
+  extra_scons_options="NSIS_CONFIG_LOG=yes"
+  extra_scons_targets="install-stubs"
 fi
 
 cd src
-scons $scons_options $scons_targets
+scons \
+  CC="${CC}" CXX="${CXX}" APPEND_CCFLAGS="${CXXFLAGS}" APPEND_LINKFLAGS="${LDFLAGS}" \
+  SKIPSTUBS=all SKIPPLUGINS=all SKIPUTILS=all SKIPMISC=all \
+  NSIS_CONFIG_CONST_DATA_PATH=yes PREFIX="$PREFIX_NSIS" \
+  -Q PATH="$PATH" $extra_scons_options \
+  install-compiler $extra_scons_targets
 
-mkdir -p $PREFIX/bin
-ln -sf $PREFIX_NSIS/bin/makensis $PREFIX_NSIS/makensis.exe
-ln -sf $PREFIX_NSIS/bin/makensis $PREFIX/bin/makensis
+mkdir -p "$PREFIX/bin"
+ln -sf "$PREFIX_NSIS/bin/makensis" "$PREFIX_NSIS/makensis.exe"
+ln -sf "$PREFIX_NSIS/bin/makensis" "$PREFIX/bin/makensis"
 
-pushd $PREFIX_NSIS
+pushd "$PREFIX_NSIS"
   mkdir share
   cd share
-  ln -sf $PREFIX_NSIS nsis
+  ln -sf "$PREFIX_NSIS" nsis
 popd
